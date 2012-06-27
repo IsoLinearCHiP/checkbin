2190456363af63a5f3c1e636cdebc2729f632f90
IsoLinearCHiP <isolin.chip@gmail.com>
2012-06-27 18:24:13 +0200

    change what should be checksummed.
    - only checksum regular files. Char-/Blockdev/Dirs shouldnt be checksummed.

--- fingerprint.sh
+++ fingerprint.sh
@@ -7,6 +7,6 @@ FILE=$1
 ## Filetype, accessrights(octal), uid, gid, size, changed time, modified time
 STAT=$(stat --printf="%f\t%a\t%u\t%g\t%s\t%z\t%y" $FILE)
 
-HASH=$([[ ! -d $FILE ]] && ( sha1sum $FILE | cut -d' ' -f1 ) || echo '-' )
+HASH=$([[ -f $FILE ]] && ( sha1sum $FILE | cut -d' ' -f1 ) || echo '-' )
 
 echo -e "$STAT\t$HASH\t$FILE"
